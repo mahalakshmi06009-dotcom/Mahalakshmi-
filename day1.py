2564404a766131676665c5c250e1b28b0549f18f
Maha-hub <mahalakshmi06009@gmail.com>
2026-09-21 21:46:31 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,13 @@
+#My First Code - Civil to UK Dream
+name = "Civil Engineer"
+dream = "UK"
+
+print(f"Hi, I'm a {name} 🌱")
+print(f"Building my dream to {dream}🇬🇧")
+
+# Chinna civil calculation - Room area
+length  = 12 #feet
+breadth = 10 #feet
+area = length *breadth
+
+print(f"My first design area is {area} sqft")
